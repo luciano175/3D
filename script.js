cfa9ab351dc3cd91f771007f1516d4fa113936fd
luciano175 <167527883+luciano175@users.noreply.github.com>
2024-08-29 11:41:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,70 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
     
     const perguntas = [
     {
-    enunciado: "Pergunta 1”,c
+    enunciado: "Pergunta 1”,
     alternativas: ["Alternativa 1”, “Alternativa 2”],
     },
     {
     enunciado: "Pergunta 2”,
     alternativas: ["Alternativa 1”, “Alternativa 2”],
     },
+    {
+    enunciado:
+    alternativas:["Alternativa1", "Alternativa2"],
+    }
     ];
+    let atual =0;
+    let )  {
+            enunciado:
+            alternativas: [
+        {
+            texto:
+            afirmação:
+        },
+        {
+            texto:
+            afirmação:
+        }
+     ]
+   },
+       function mostraAlternativa () {
+        for (const alternativa of perguntaAtual.alternativas)
+      }
+       {
+            const botaoAlternativas = document.
+    createElement("butom");
+              botaoAlternativas.textContent = alternativa;
+              caixaAlternativas.appendChild(botaoAlternativas);
+       }
+    }
+          botaoAlternativas.textContent = alternativa;
+          botaoAlternativas.textContent = alternativa.texto;
+          botaoAlternativas.addEventListener("click");
+          botaoAlternativas.addEventListener("click", function ()  {
+
+             });
+
+
+              let atual = 0;
+        function mostraPergunta () {
+            perguntaAtual = peruntas [atual];
+        botaoAlternativas,addEventListener("click", function () {
+                       atual++;
+            mostraPergunta();
+
+        });
+    }
+
+           function mostraAlternativa () {
+             for(const alternativa of perguntaAtual.alternativas)  {
+                const botaoAlternativas = document.
+            createElement("buton");
+                botaoAlternativas.textContent = alternativa.texto;
+                botaoAlternativas.addEventListener("click", function () {
+                    atual++;
+                    mostraPergunta();
+                 });
+                 caixaAlternativas.appendChild(botaoAlternativas);
+             }
+
+           }
